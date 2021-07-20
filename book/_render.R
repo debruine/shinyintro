bea1ff1e123224bbbab535f48de1aabadf13ed01
debruine <debruine@gmail.com>
2021-07-20 17:01:44 +0100

    Typos through Ch 7

--- book/_render.R
+++ book/_render.R
@@ -4,7 +4,8 @@ setwd("book")
 preview <- FALSE 
 
 # render a chapter or the whole book
-#browseURL(bookdown::preview_chapter("13_modules.Rmd"))
+#browseURL(bookdown::preview_chapter("02_shinydashboard.Rmd"))
+
 
 # make PDF
 browseURL(bookdown::render_book("index.Rmd", "bookdown::pdf_book", preview = preview))
